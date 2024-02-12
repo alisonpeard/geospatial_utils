81f26072f5bfedc97900bc6923223432dea9c1df
Alison Peard <alison@Alisons-MacBook-Pro.local>
2024-02-12 20:11:49 +0000

    Simple field generating function for numpy

--- geospatial_utils/__init__.py
+++ geospatial_utils/__init__.py
@@ -7,6 +7,7 @@ Package for commonly used geospatial functions
 from .utils import *
 from .gridding import *
 from .xarray import *
+from .fields import *
 
 __version__ = "0.01"
 __author__ = """Alison Peard"""
