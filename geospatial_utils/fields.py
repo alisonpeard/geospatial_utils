07ba9dd9fcc99f926ee61d46d8817b0638bb83ba
Alison Peard <alison@Alisons-MacBook-Pro.local>
2024-02-26 15:44:48 +0000

    matern grf from kl expansion

--- geospatial_utils/fields.py
+++ geospatial_utils/fields.py
@@ -4,7 +4,8 @@ import numpy as np
 from numbers import Number
 from itertools import product
 from scipy.spatial import distance_matrix
-from scipy.special import gamma, kv
+from scipy.fft import idctn
+# from scipy.special import gamma, kv
 
 
 def cosine_cov(dx, l=1):
@@ -13,12 +14,6 @@ def cosine_cov(dx, l=1):
 
 def exp1d_cov(dx, l=0.1, σ=1):
     return σ * np.exp( -dx / l)
-
-
-def matern_cov(dx, l=1, nu=.5, eps=1e-5):
-    x = np.sqrt(2 * nu) * dx / l
-    C = (1 / (gamma(nu) * 2**(nu-1))) * (x**nu) * kv(nu, x + eps)
-    return C
 
 
 def gaussian_random_field(nx, ny, l=10, mean=0, var=1, cov=cosine_cov, **kwargs):
@@ -31,3 +26,22 @@ def gaussian_random_field(nx, ny, l=10, mean=0, var=1, cov=cosine_cov, **kwargs)
     cmat = cov(dmat, l=l, **kwargs)
     grf = np.random.multivariate_normal(mean, var * cmat)
     return grf.reshape([ny, nx])
+
+
+def matern_grf(n, tau, alpha):
+    """Return a sample of a Gaussian random field on [0,1]^2.
+    
+    with:
+    mean 0
+    covariance operator C = (-Delta + tau^2)^(-alpha)
+    where Delta is the Laplacian with zero Neumann boundary conditions.
+    
+    adapted from: https://github.com/neuraloperator/neuraloperator/blob/master/data_generation/darcy/GRF.m
+    """
+    xi = np.random.normal(0, 1, (n, n))
+    K1, K2 = np.meshgrid(range(n), range(n))
+    coef = tau**(alpha-1) * (np.pi**2 * (K1**2 + K2**2) + tau**2)**(-alpha/2)
+    L = n * coef * xi
+    L[0, 0] = 0
+    U = idctn(L, norm='ortho')
+    return U
